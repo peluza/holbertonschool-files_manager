4ec3b81094c3356e2d7a4089cebaccb85283748d
kimha1030 <1478@holbertonschool.com>
2021-06-07 17:55:02 -0500

    Delete request

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,8 @@ const router = (app) => {
   const path = Router();
   app.use(json());
   app.use('/', path);
-  path.get('/status', (request, response) => AppController.getStatus(request, response));
-  path.get('/stats', (request, response) => AppController.getStats(request, response));
+  path.get('/status', (request, response) => AppController.getStatus(response));
+  path.get('/stats', (request, response) => AppController.getStats(response));
 };
 
 export default router;
