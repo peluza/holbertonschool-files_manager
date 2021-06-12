04bb8231199d986d4cdc5d9ad549e06055502d34
kimha1030 <1478@holbertonschool.com>
2021-06-12 16:44:29 -0500

    Fix the conditionalabout if the parentid is different to zero

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -27,12 +27,13 @@ class FilesController {
     if (!type || ['folder', 'file', 'image'].includes(type)) {
       return response.status(400).send({ error: 'Missing type' });
     }
-    if (!data && ['file', 'image'].includes(type)) {
+    if (!data && type !== 'folder') {
       return response.status(400).send({ error: 'Missing data' });
     }
     const isFilePublic = isPublic || false;
     const parId = parentId || 0;
-    if (parId !== 0) {
+    const value = parseInt('0', 10);
+    if (parId !== value) {
       const resultFiles = await dbClient.DB.collection('files').findOne({ _id: ObjectId(parId) });
       if (!resultFiles) {
         return response.status(400).send({ error: 'Parent not found' });
@@ -40,37 +41,37 @@ class FilesController {
       if (!['folder'].includes(resultFiles.type)) {
         return response.status(400).send({ error: 'Parent is not a folder' });
       }
-      const info = {
-        name: `${name}`, type: `${type}`, parentId: `${parentId}`, isPublic: `${isPublic}`, userId: `${result._id}`,
-      };
-      if (['folder'].includes(resultFiles.type)) {
-        await dbClient.DB.collection('files').insertOne(info);
-        return response.status(201).send(info);
-      }
-      const path = process.env.FOLDER_PATH || '/tmp/files_manager';
-      const nameFile = uuidv4();
-      const fullFile = `${path}/${nameFile}`;
-      const buf = Buffer.from(data, 'base64');
-      await fs.mkdir(path, (err) => {
-        if (err) {
-          return response.status(400).send({ error: `${err.message}` });
-        }
-        return true;
-      });
-      await fs.writeFile(fullFile, buf, (err) => {
-        if (err) {
-          return response.status(400).send({ error: `${err.message}` });
-        }
-        return true;
-      });
-      const infoFI = {
-        name: `${name}`, type: `${type}`, parentId: `${parentId}`, isPublic: `${isPublic}`, userId: `${result._id}`, localPath: `${fullFile}`,
-      };
-      if (['file', 'image'].includes(resultFiles.type)) {
-        await dbClient.DB.collection('files').insertOne(infoFI);
-        return response.status(201).send(infoFI);
-      }
     }
+    const info = {
+      name: `${name}`, type: `${type}`, parentId: `${parentId}`, isPublic: `${isFilePublic}`, userId: `${result._id}`,
+    };
+    if (['folder'].includes(type)) {
+      await dbClient.DB.collection('files').insertOne(info);
+      return response.status(201).send(info);
+    }
+    const path = process.env.FOLDER_PATH || '/tmp/files_manager';
+    const nameFile = uuidv4();
+    const fullFile = `${path}/${nameFile}`;
+    const buf = Buffer.from(data, 'base64');
+    await fs.mkdir(path, (err) => {
+      if (err) {
+        return response.status(400).send({ error: `${err.message}` });
+      }
+      return true;
+    });
+    await fs.writeFile(fullFile, buf, (err) => {
+      if (err) {
+        return response.status(400).send({ error: `${err.message}` });
+      }
+      return true;
+    });
+    const infoFI = {
+      name: `${name}`, type: `${type}`, parentId: `${parentId}`, isPublic: `${isPublic}`, userId: `${result._id}`, localPath: `${fullFile}`,
+    };
+    if (['file', 'image'].includes(type)) {
+      await dbClient.DB.collection('files').insertOne(infoFI);
+    }
+    return response.status(201).send(infoFI);
   }
 }
 
